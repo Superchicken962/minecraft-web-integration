241b2a3d5d7ddbb84b4a76fb72073ac237d6f45a
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-10-07 15:12:54 +1030

    Add format function for project updater

--- integration/classes/ProjectUpdater.js
+++ integration/classes/ProjectUpdater.js
@@ -1,6 +1,10 @@
 const { gitP } = require("simple-git");
+const fs = require("node:fs");
+const path = require("node:path");
 
 class ProjectFileUpdater {
+    #rootDir = path.join(__dirname, "../");
+
     /**
      * @param { String[] } ignoreFiles - Files to ignore. 
      */
@@ -10,8 +14,37 @@ class ProjectFileUpdater {
     }
 
     async #clone() {
+        const clonePath = path.join(this.#rootDir, "clone");
+
+        // If clone files are still there, clear it.
+        if (fs.existsSync(clonePath)) {
+            await fs.promises.rm(clonePath);
+        }
+
         const git = gitP();
-        await git.clone(this.repo, path.join(__dirname, "clone"));
+        await git.clone(this.repo, clonePath);
+    }
+
+    async #formatFiles() {
+        const basePath = path.join(this.#rootDir, "clone/integration")
+
+        // Delete files specified in "ignore files".
+        for (const fileName of this.ignoreFiles) {
+            const pth = path.join(basePath, fileName);
+    
+            if (fs.existsSync(pth)) {
+                await fs.promises.unlink(pth);
+            }
+        }
+    }
+
+    async #replaceFiles() {
+        const clonePath = path.join(this.#rootDir, "clone");
+
+        // Copy the current directory as a backup.
+        await fs.promises.cp(this.#rootDir, path.join(this.#rootDir, "backup"), { recursive: true });
+
+        console.log(clonePath);
     }
 
     /**
@@ -21,9 +54,14 @@ class ProjectFileUpdater {
      * @returns { Promise }
      */
     async update(onprogress) {
-        onprogress?.("Cloning files...");
+        onprogress?.("Cloning from GitHub...");
         await this.#clone();
-        onprogress?.("...");
+
+        onprogress?.("Formatting...");
+        await this.#formatFiles();
+
+        onprogress?.("Replacing files...");
+        await this.#replaceFiles();
     }
 }
 
